95630a9ca87c7745d2d129051b95f5a147d0d962
professoreugenio <professoreugeniomls@gmail.com>
2026-08-06 16:33:34 -0300

    menu:Sobre

--- SistemaControlededeEstoque/src/util/Formatador.java
+++ SistemaControlededeEstoque/src/util/Formatador.java
@@ -6,13 +6,18 @@ import java.util.Locale;
 
 public class Formatador {
 
+    private static final Locale LOCALE_BRASIL =
+            Locale.forLanguageTag("pt-BR");
+
     public static String moeda(BigDecimal valor) {
 
         if (valor == null) {
             valor = BigDecimal.ZERO;
         }
 
-        NumberFormat formato = NumberFormat.getCurrencyInstance(Locale.of("pt", "BR"));
+        NumberFormat formato =
+                NumberFormat.getCurrencyInstance(LOCALE_BRASIL);
+
         return formato.format(valor);
     }
 }
